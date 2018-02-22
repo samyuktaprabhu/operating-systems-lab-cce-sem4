feda12b7f4424a0ab5a24277af3f7c0ed127b0da
M Samyuktha Prabhu <samyuktaprabhu@gmail.com>
2018-02-22 03:33:37 +0000

    Create lab5Q1.c

---
+++ lab5Q1.c
@@ -0,0 +1,19 @@
+```
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+int main() {
+    if(fork()==0)
+    {
+    printf("Child Process is running. \n");
+    printf("getpid : %d getppid : %d \n",getpid(),getppid());
+    }
+    else
+    {
+    printf("Parent Process is running. \n");
+    printf("getpid : %d getppid : %d \n",getpid(),getppid());
+    }
+    return 0;
+}
+
+```
