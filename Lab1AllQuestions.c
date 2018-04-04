3ef23325c0fe54bf938011d6b0f74ee8013d32ca
M Samyuktha Prabhu <samyuktaprabhu@gmail.com>
2018-04-04 15:46:42 +0530

    Create Lab1AllQuestions.c

---
+++ Lab1AllQuestions.c
@@ -0,0 +1,35 @@
+
+Q1. ls - Web newfil anaconda3 newfile
+A-assignment-sammy    ...
+
+ls -l-drwxrwxr-x  2 sammy sammy      4096 Dec 10 18:01 Web   ...
+
+ls -l --color drwxrwxr-x  2 sammy sammy    4096 Dec 10 18:01 Web
+
+cat file1.txt - hello world 
+
+cat -n file1.txt - 1 hello world
+
+pwd- prints the current working directory example home/sammy
+
+ls newfile - innerfile
+
+ls -l - all permissions
+
+ls -a  all files including hidden files
+
+ls -ld - drwxr-xr-x 64 sammy sammy 4096 Jan 24 14:18 .
+
+ls /usr/bin/d* - all files starting with d in the current folder
+
+#(continue with lab manual page #7)
+
+
+Q3.(i) find *.txt
+
+(ii) ls *[0-9]*
+
+(iii) find . -name "????*"
+
+(iv) ls -v [!a,e,i,o,u]*
+
